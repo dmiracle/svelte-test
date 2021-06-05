245fc33a714bda52bef81715412917207c3de446
Dylan Miracle <dylan.miracle@gmail.com>
2021-06-05 18:56:41 -0500

    add chart

--- src/App.svelte
+++ src/App.svelte
@@ -20,32 +20,10 @@
 	});
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div bind:this={el} class="chart"></div>
 
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}	
 	.chart :global(div) {
 		font: 10px sans-serif;
 		background-color: steelblue;
